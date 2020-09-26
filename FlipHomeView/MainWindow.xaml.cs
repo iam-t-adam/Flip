506b459700a55271b004c33cbc9a787f1a6e9e5a
iam-t-adam <tadam.space@gmail.com>
2020-09-26 15:43:54 +0200

    Add WPF Application, Menu, Added function to File > Close, Window > About, About Window OK button. Updated to version 1.1.0.

---
+++ FlipHomeView/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace FlipHomeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var AB = new AboutWindow();
+            AB.Show();
+        }
+    }
+}
